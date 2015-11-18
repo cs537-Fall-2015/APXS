a2cbac6eadd66ba7d9c009b15d15c60c529bf1b0
ed12al <ed12al@hotmail.com>
2015-11-17 17:33:20 -0800

    more

--- src/json/Constants.java
+++ src/json/Constants.java
@@ -5,4 +5,5 @@ public final class Constants {
 	public static final int APXS_PORT = 9017;
 	public static final String commands = "src\\APXS\\APXS_testMain\\commands\\";
 	public static final String apxs_logpath = "src\\APXS\\module\\log.txt";
+	public static final String apxs_savelog = "src\\APXS\\APXS_testMain\\log.txt";
 }
