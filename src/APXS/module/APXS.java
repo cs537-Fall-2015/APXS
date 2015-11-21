69d1a9b298ab7ce967925bf2b54d8a092e1b3641
soumikshah <soumik.shah@gmail.com>
2015-11-20 19:46:00 -0800

    GUI bug fixes

--- src/APXS/module/APXS.java
+++ src/APXS/module/APXS.java
@@ -59,6 +59,8 @@ public class APXS {
 			cmd5.setBackground(Color.red);
 			cmd1.setBackground(Color.white);
 			cmd6.setBackground(Color.white);
+			cmd3.setBackground(Color.white);
+	    	cmd2.setBackground(Color.white);
 			threadSleep(3000);
 			log.println("APXS OFF -" +dateFormat.format(new Date()));
 			log.println("");
@@ -221,6 +223,7 @@ public class APXS {
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
 	       // frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	        frame.setVisible( true );
+	        frame.setLocationRelativeTo(null);
 	        flag2++;
 			}
 			if (message.equalsIgnoreCase("APXS_ON")){
@@ -231,7 +234,7 @@ public class APXS {
 				turnOff();
 				return message;
 			}else if (message.equalsIgnoreCase("CHECK_TEMPERATURE")){
-				return checkTemp()? "temperature meets working requirement" : "temperature does not meet working requirement";
+				return checkTemp()? "Temperature meets working requirement" : "temperature does not meet working requirement";
 			}else if(message.equalsIgnoreCase("APXS_MEASURE")){
 				run();
 				return message;
